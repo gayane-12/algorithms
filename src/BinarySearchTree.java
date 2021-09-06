cac5561b8c40437e6fba8d185d2940d8f02da8be
Gayane_1 <gmiranyan1@gmail.com>
2021-09-06 18:45:51 +0400

    first commit

---
+++ src/BinarySearchTree.java
@@ -0,0 +1,96 @@
+class BinarySearchTree {
+    class Node {
+        int key;
+        Node left, right;
+
+        public Node(int item) {
+            key = item;
+            left = right = null;
+        }
+    }
+
+    Node root;
+    BinarySearchTree() {
+        root = null;
+    }
+
+    void insert(int key) {
+        root = insertKey(root, key);
+    }
+
+    Node insertKey(Node root, int key) {
+        if (root == null) {
+            root = new Node(key);
+            return root;
+        }
+
+        if (key < root.key)
+            root.left = insertKey(root.left, key);
+        else if (key > root.key)
+            root.right = insertKey(root.right, key);
+
+        return root;
+    }
+
+    void inorder() {
+        inorderRec(root);
+    }
+
+    void inorderRec(Node root) {
+        if (root != null) {
+            inorderRec(root.left);
+            System.out.print(root.key + " -> ");
+            inorderRec(root.right);
+        }
+    }
+
+    void deleteKey(int key) {
+        root = deleteRec(root, key);
+    }
+
+    Node deleteRec(Node root, int key) {
+        if (root == null)
+            return root;
+
+        if (key < root.key)
+            root.left = deleteRec(root.left, key);
+        else if (key > root.key)
+            root.right = deleteRec(root.right, key);
+        else {
+            if (root.left == null)
+                return root.right;
+            else if (root.right == null)
+                return root.left;
+
+            root.key = minValue(root.right);
+            root.right = deleteRec(root.right, root.key);
+        }
+
+        return root;
+    }
+
+    int minValue(Node root) {
+        int minv = root.key;
+        while (root.left != null) {
+            minv = root.left.key;
+            root = root.left;
+        }
+        return minv;
+    }
+
+    public static void main(String[] args) {
+        BinarySearchTree tree = new BinarySearchTree();
+        tree.insert(30);
+        tree.insert(6);
+        tree.insert(32);
+        tree.insert(21);
+        tree.insert(2);
+        tree.insert(1);
+        tree.insert(13);
+        tree.insert(6);
+
+        tree.inorder();
+        tree.deleteKey(32);
+        tree.inorder();
+    }
+}
